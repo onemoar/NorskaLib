c918836cf017972f9244d40857ee488710761d1a
NorskaGames <norskagames@gmail.com>
2022-09-22 19:50:45 +0400

    Improving UI tools
    - added tab system
    - minor naming refactoring

--- UI/Common/ValueText/ValueText.cs
+++ UI/Common/ValueText/ValueText.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using TextField = TMPro.TextMeshProUGUI;
+using Label = TMPro.TextMeshProUGUI;
 
 namespace NorskaLib.UI
 {
 	public class ValueText : MonoBehaviour
 	{
-		[SerializeField] TextField label;
+		[SerializeField] Label label;
 		public string textFormat;
 
 		[Space]
 		[SerializeField] AnimationCurve curve;
+
+		private System.Action routineCallback;
+		private Coroutine animationRoutine;
 
 		#region MonoBehaviour
 
@@ -23,6 +26,13 @@ namespace NorskaLib.UI
 
 		#endregion
 
+		private void SetText(int value)
+		{
+			label.text = string.IsNullOrEmpty(textFormat)
+				? value.ToString()
+				: string.Format(textFormat, value);
+		}
+
 		/// <summary>
 		/// Sets the label text using provided format instantly.
 		/// </summary>
@@ -32,26 +42,29 @@ namespace NorskaLib.UI
 
 			SetText(value);
 		}
-
-		private Coroutine countAnimatioRoutine;
 		/// <summary>
 		/// Sets the label text using provided format over given time.
 		/// </summary>
-		public void DisplayAnimated(int from, int to, float duration = 0.3f, float delay = 0.0f)
+		public void DisplayAnimated(int from, int to, float duration = 0.3f, float delay = 0.0f, System.Action callback = null)
 		{
 			IEnumerator Routine()
 			{
-				var t = 0f;
+				var time = 0f;
 
-				yield return delay > 0
-					? new WaitForSeconds(delay)
-					: null;
+				if (delay > 0)
+					while(time < delay)
+                    {
+						time += Time.deltaTime;
+						yield return null;
+					}
 
-				while (t < duration)
+
+				time = 0f;
+				while (time < duration)
 				{
-					t += Time.deltaTime;
+					time += Time.deltaTime;
 
-					var factor = curve.Evaluate(t / duration);
+					var factor = curve.Evaluate(time / duration);
 					var value = Mathf.RoundToInt(Mathf.Lerp(from, to, factor));
 
 					SetText(value);
@@ -61,28 +74,25 @@ namespace NorskaLib.UI
 
 				SetText(to);
 
-				countAnimatioRoutine = null;
+				animationRoutine = null;
+				routineCallback?.Invoke();
 			}
 
 			Break();
 
-			countAnimatioRoutine = StartCoroutine(Routine());
-		}
-
-		private void SetText(int value)
-		{
-			label.text = string.IsNullOrEmpty(textFormat)
-				? value.ToString()
-				: string.Format(textFormat, value);
+			routineCallback = callback;
+			animationRoutine = StartCoroutine(Routine());
 		}
 
 		/// <summary>
-		/// Stops the animation.
+		/// Stops the animation instantly without completing it or invoking final callback.
 		/// </summary>
 		public void Break()
 		{
-			if (countAnimatioRoutine != null)
-				StopCoroutine(countAnimatioRoutine);
+			if (animationRoutine != null)
+				StopCoroutine(animationRoutine);
+
+			routineCallback = null;
 		}
 	}
 }
